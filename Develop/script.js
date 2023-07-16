cf579618e77396a59e26544c7a4343076ae985c9
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-07-16 13:32:59 -0500

    Add line that can show a user when something is saved

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  // The code below saves various variables that I will need throughout this file.
   var hour9 = $('#hour-9');
   var hour10 = $('#hour-10');
   var hour11 = $('#hour-11');
@@ -14,12 +15,12 @@ $(function () {
   var currentDayEl = $('#currentDay');
   var currentDay = dayjs().format('dddd, MMMM DD');
   var saveButton = $('.saveBtn');
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  var saved = $('#saved');
+
+  // The code below adds a listener for click events on the save button. This code
+  // uses the id in the containing time-block as a key to save the user input in
+  // local storage. It then uses `this` reference its parent container using DOM
+  // traversal and check for each button's ID to determine which element to act on.
   saveButton.click(function() {
     if(this.id == 'btn9'){
       localStorage.setItem('hour9txt', hour9txt.value);
@@ -40,14 +41,21 @@ $(function () {
     } else if (this.id == 'btn17') {
       localStorage.setItem('hour17txt', hour17txt.value);
     }
+    // Below displays a message at the top when the save button is pressed
+    // letting the user know that the item has been saved.  It then disappears
+    // after 2 seconds.
+    saved.text('Saved!');
+    setTimeout(() => {
+      saved.text('');
+    }, 2000)
   });
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+  
+
+  //  The code below saves the current time (in hours) with the help of dayjs
+  // to a variable and checks that time to be lesser than, equal to, or greater 
+  // than the time blocks in the calendar's elements, then allowing it to 
+  // determine which blocks need which colors depending on if they are in the 
+  // past, present, or future.
   var currentTime = dayjs().format('H');
 
   if(currentTime < 9) {
